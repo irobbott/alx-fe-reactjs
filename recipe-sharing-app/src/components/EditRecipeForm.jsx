0dc957600364b56d8ceb6cf63bfc86685ba68b6b
Iconic Robbott <dev.robbott@gmail.com>
2026-01-25 13:16:55 +0100

    refactor: standardize event parameter naming in EditRecipeForm component

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -7,8 +7,8 @@ const EditRecipeForm = ({ recipe }) => {
   const [title, setTitle] = useState(recipe.title);
   const [description, setDescription] = useState(recipe.description);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+const handleSubmit = (event) => {
+  event.preventDefault();
 
     updateRecipe({
       ...recipe,
@@ -36,7 +36,5 @@ const EditRecipeForm = ({ recipe }) => {
     </form>
   );
 };
-
-// event.preventDefault();
 
 export default EditRecipeForm;
